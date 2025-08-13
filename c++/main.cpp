a2f4d3b275b3c96839a64a48332bd7bdb1f1285d
MrKleivan <163395188+MrKleivan@users.noreply.github.com>
2025-08-13 11:02:23 +0200

    getg

---
+++ c++/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    auto result = (10 <= 20) > 0;
+    std::cout << result << std::endl;
+    std::cout << "Hello, world!\n";
+    std::cout << "Yoo!!";   
+    std::cout << "Yofdfvo!!";   
+}
